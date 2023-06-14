f3f31f37af7897eca1ef0dcd944052f46e669ee3
Prince Avecillas <avecillasprince@yahoo.com>
2023-06-13 21:52:11 -0400

    Adding medium sliding

---
+++ swift/Neetcode/SlidingWindow/Medium/209.MinimumSubarraySum.swift
@@ -0,0 +1,25 @@
+//Given an array of positive integers nums and a positive integer target, 
+//return the minimal length of a subarray whose sum is greater than or equal to target. 
+//If there is no such subarray, return 0 instead.
+
+func minSubArrayLen(_ target: Int, _ nums: [Int]) -> Int {
+    var left = 0
+    var currentSum = 0
+    var result = Int.max
+
+    for i in 0..<nums.count {
+        currentSum += nums[i]
+
+        while currentSum >= target {
+            result = min(result, i - left + 1)
+            currentSum -= nums[left]
+            left += 1
+        }
+    }
+
+    return result == Int.max ? 0 : result
+}
+
+print(minSubArrayLen(7, [2,3,1,2,4,3]))
+print(minSubArrayLen(4, [1,4,4]))
+print(minSubArrayLen(11, [1,1,1,1,1,1,1,1]))
