e8336b5ccc7ea5b230b0199ed32e5e3e541f1823
Prince Avecillas <avecillasprince@yahoo.com>
2023-11-17 16:13:59 -0500

    Adding bs

---
+++ swift/Neetcode/BinarySearch/Medium/81.SearchRotatedII.swift
@@ -0,0 +1,33 @@
+// Given the array nums after the rotation and an integer target, 
+// return true if target is in nums, or false if it is not in nums.
+
+func search(_ nums: [Int], _ target: Int) -> Bool {
+    var left = 0, right = nums.count - 1
+
+    while left <= right {
+        let mid = (right + left) / 2
+
+        if nums[mid] == target {
+            return true
+        } else if nums[left] < nums[mid] {
+            if nums[left] <= target, target < nums[mid] {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        } else if nums[left] > nums[mid] {
+            if nums[mid] < target, target <= nums[right] {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        } else {
+            left += 1
+        }
+    }
+
+    return false
+}
+
+print(search([2,5,6,0,0,1,2], 0))
+print(search([2,5,6,0,0,1,2], 3))
