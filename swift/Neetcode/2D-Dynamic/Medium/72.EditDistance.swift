c0b9029e4edba22af382228398f80c2e44c80097
Prince Avecillas <avecillasprince@yahoo.com>
2023-11-26 13:08:40 -0500

    Adding dp

---
+++ swift/Neetcode/2D-Dynamic/Medium/72.EditDistance.swift
@@ -0,0 +1,55 @@
+// Given two strings word1 and word2, return the 
+// minimum number of operations required to convert word1 to word2.
+// You have the following three operations permitted on a word:
+// Insert a character Delete a character Replace a character
+
+func minDistance(_ word1: String, _ word2: String) -> Int {
+    if word1.isEmpty { return word2.count }
+    if word2.isEmpty { return word1.count }
+
+    let word1 = Array(word1), word2 = Array(word2)
+    var tab = Array(repeating: Array(repeating: 0, count: word2.count + 1), count: word1.count + 1)
+
+    for i in 0...word1.count { tab[i][0] = i }
+    for j in 0...word2.count { tab[0][j] = j }
+
+    for i in 1...word1.count {
+        for j in 1...word2.count {
+            if word1[i - 1] == word2[j - 1] {
+                tab[i][j] = tab[i - 1][j - 1]
+            } else {
+                tab[i][j] = min(tab[i - 1][j], tab[i][j - 1], tab[i - 1][j - 1]) + 1
+            }
+        }
+    }
+
+    return tab[word1.count][word2.count]
+}
+
+print(minDistance("horse", "ros"))
+print(minDistance("intention", "execution"))
+
+
+func minDistance2(_ word1: String, _ word2: String) -> Int {
+    let word1 = Array(word1), word2 = Array(word2)
+    var memo = [String: Int]()
+
+    func dp(_ i: Int, _ j: Int) -> Int {
+        let key = "\(i),\(j)"
+
+        if let value = memo[key] { return value }
+        if i >= word1.count { return word2.count - j }
+        if j >= word2.count { return word1.count - i }
+        if word1[i] == word2[j] { return dp(i + 1, j + 1) }
+
+        let insert = dp(i, j + 1)
+        let delete = dp(i + 1, j)
+        let replace = dp(i + 1, j + 1)
+        let result = min(insert, delete, replace) + 1
+
+        memo[key] = result
+        return result
+    }   
+
+    return dp(0, 0)
+}
---
+++ swift/Neetcode/2D-Dynamic/Medium/926.FlipStringToMonotone.swift
@@ -0,0 +1,68 @@
+// A binary string is monotone increasing if it consists of some number of 0's 
+// (possibly none), followed by some number of 1's (also possibly none).
+// You are given a binary string s. You can flip s[i] changing it 
+// from 0 to 1 or from 1 to 0.
+// Return the minimum number of flips to make s monotone increasing.
+
+func minFlipsMonoIncr(_ s: String) -> Int {
+    let s = Array(s)
+    var tab = Array(repeating: Array(repeating: 0, count: 2), count: s.count + 1)
+
+    for i in 1...s.count {
+        let isOne = s[i - 1] == "1" ? 1 : 0
+
+        tab[i][0] = tab[i - 1][0] + isOne
+        tab[i][1] = min(tab[i - 1][0], tab[i - 1][1]) + (1 - isOne)
+    }
+
+    return min(tab[s.count][0], tab[s.count][1])
+}
+
+print(minFlipsMonoIncr("00110"))
+print(minFlipsMonoIncr("010110"))
+print(minFlipsMonoIncr("00011000"))
+
+
+func minFlipsMonoIncr2(_ s: String) -> Int {
+    var ones = 0, result = 0
+
+    for number in s {
+        if number == "1" {
+            ones += 1
+        } else {
+            result = min(result + 1, ones)
+        }
+    }
+
+    return result
+}
+
+
+func minFlipsMonoIncr3(_ s: String) -> Int {
+    let s = Array(s)
+    var memo = [String: Int]()
+
+    func dp(_ i: Int, _ sawOne: Bool) -> Int {
+        let key = "\(i),\(sawOne)"
+
+        if let value = memo[key] { return value }
+        if i >= s.count { return 0 }
+
+        var result = 0
+
+        if sawOne {
+            result = dp(i + 1, sawOne) + (s[i] == "1" ? 0 : 1)
+        } else {
+            if s[i] == "0" {
+                result = min(dp(i + 1, false), dp(i + 1, true))
+            } else {
+                result = min(dp(i + 1, true), dp(i + 1, false) + 1)
+            }
+        }
+
+        memo[key] = result
+        return result
+    }
+
+    return dp(0, false)
+}
