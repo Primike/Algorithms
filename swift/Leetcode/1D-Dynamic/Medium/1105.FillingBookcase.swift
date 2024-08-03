56bd352431ed3051eecdb6999d71a8a6ff16e3c4
Prince Avecillas <avecillasprince@yahoo.com>
2024-08-03 15:36:21 -0400

    DP

---
+++ swift/Leetcode/1D-Dynamic/Medium/1105.FillingBookcase.swift
@@ -0,0 +1,59 @@
+// You are given an array books where books[i] = [thicknessi, heighti] 
+// indicates the thickness and height of the ith book. 
+// You are also given an integer shelfWidth.
+// We want to place these books in order onto bookcase shelves 
+// that have a total width shelfWidth.
+// For example, if we have an ordered list of 5 books, 
+// we might place the first and second book onto the first shelf, 
+// the third book on the second shelf, and the fourth and fifth book on the last shelf.
+// Return the minimum possible height that the total bookshelf 
+// can be after placing shelves in this manner.
+
+// Time: O(n * w), Space: O(n)
+func minHeightShelves(_ books: [[Int]], _ shelfWidth: Int) -> Int {
+    var tab = Array(repeating: Int.max, count: books.count + 1)
+    tab[books.count] = 0
+    
+    for i in (0..<books.count).reversed() {
+        var j = i, width = 0
+        var maxHeight = 0
+
+        while j < books.count, width + books[j][0] <= shelfWidth {
+            width += books[j][0]
+            maxHeight = max(maxHeight, books[j][1])
+            tab[i] = min(tab[i], tab[j + 1] + maxHeight)
+            j += 1
+        }
+    }
+
+    return tab[0]
+}
+
+print(minHeightShelves([[1,1],[2,3],[2,3],[1,1],[1,1],[1,1],[1,2]], 4))
+print(minHeightShelves([[1,3],[2,4],[3,2]], 6))
+
+
+func minHeightShelves(_ books: [[Int]], _ shelfWidth: Int) -> Int {
+    var memo = [Int: Int]()
+
+    func dp(_ index: Int) -> Int {
+        if index == books.count { return 0 }
+        if let value = memo[index] { return value }
+
+        var result = Int.max
+        var i = index, width = 0
+        var maxHeight = 0
+
+        while i < books.count, width + books[i][0] <= shelfWidth {
+            width += books[i][0]
+            maxHeight = max(maxHeight, books[i][1])
+            result = min(result, dp(i + 1) + maxHeight)
+            i += 1
+        }
+
+        memo[index] = result
+        return result
+    }
+
+    return dp(0)
+}
---
+++ swift/Leetcode/2D-Dynamic/Medium/1230.TossStrangeCoins.swift
@@ -0,0 +1,28 @@
+// You have some coins.  The i-th coin has a probability 
+// prob[i] of facing heads when tossed.
+// Return the probability that the number of coins 
+// facing heads equals target if you toss every coin exactly once.
+
+func probabilityOfHeads(_ prob: [Double], _ target: Int) -> Double {
+    var memo = [String: Double]()
+    
+    func dp(_ i: Int, _ target: Int) -> Double {
+        let key = "\(i),\(target)"
+
+        if target < 0 { return 0.0 }
+        if i == prob.count { return target == 0 ? 1.0 : 0.0 }
+        if let value = memo[key] { return value }
+        
+        let heads = dp(i + 1, target - 1) * prob[i]
+        let tails = dp(i + 1, target) * (1.0 - prob[i])
+        let result = heads + tails
+        
+        memo[key] = result
+        return result
+    }
+    
+    return dp(0, target)
+}
+
+print(probabilityOfHeads([0.4], 1))
+print(probabilityOfHeads([0.5,0.5,0.5,0.5,0.5], 0))
