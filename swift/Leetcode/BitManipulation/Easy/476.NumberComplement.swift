1876cb96481e17a39ac00adf56198a0bf17d00d7
Prince Avecillas <avecillasprince@yahoo.com>
2024-09-02 22:30:10 -0400

    Bit

---
+++ swift/Leetcode/BitManipulation/Easy/476.NumberComplement.swift
@@ -0,0 +1,16 @@
+// The complement of an integer is the integer you get 
+// when you flip all the 0's to 1's and all the 1's to 0's 
+// in its binary representation.
+// For example, The integer 5 is "101" in binary and 
+// its complement is "010" which is the integer 2.
+// Given an integer num, return its complement.
+
+func findComplement(_ num: Int) -> Int {
+    let bitLength = Int.bitWidth - num.leadingZeroBitCount
+    let mask = (1 << bitLength) - 1
+
+    return num ^ mask
+}
+
+print(findComplement(5))
+print(findComplement(1))
