c072ff5385ccc0df25ac03b946b358969fc4816a
Prince Avecillas <avecillasprince@yahoo.com>
2025-01-09 22:24:56 -0500

    Prefix

---
+++ swift/Leetcode/PrefixSum/Medium/3152.SpecialArrayII.swift
@@ -0,0 +1,33 @@
+// An array is considered special if every pair of its 
+// adjacent elements contains two numbers with different parity.
+// You are given an array of integer nums and a 2D integer matrix queries, 
+// where for queries[i] = [fromi, toi] your task is to check that
+// subarray nums[fromi..toi] is special or not.
+// Return an array of booleans answer such that answer[i] is true 
+// if nums[fromi..toi] is special.
+
+func isArraySpecial(_ nums: [Int], _ queries: [[Int]]) -> [Bool] {
+    var result = Array(repeating: false, count: queries.count)
+    var prefixArray = Array(repeating: 0, count: nums.count)
+    prefixArray[0] = 0
+
+    for i in 1..<nums.count {
+        if nums[i] % 2 == nums[i - 1] % 2 {
+            prefixArray[i] = prefixArray[i - 1] + 1
+        } else {
+            prefixArray[i] = prefixArray[i - 1]
+        }
+    }
+
+    for i in 0..<queries.count {
+        let query = queries[i]
+        let start = query[0]
+        let end = query[1]
+        result[i] = prefixArray[end] - prefixArray[start] == 0
+    }
+
+    return result
+}
+
+print(isArraySpecial([3,4,1,2,6], [[0,4]]))
+print(isArraySpecial([4,3,1,6], [[0,2],[2,3]]))
