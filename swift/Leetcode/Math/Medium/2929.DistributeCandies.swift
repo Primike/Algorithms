71ec0a98bfd2c66a57a10ab50089baa34d86afc7
Prince Avecillas <avecillasprince@yahoo.com>
2025-06-02 22:08:03 -0400

    Math

---
+++ swift/Leetcode/Math/Medium/2929.DistributeCandies.swift
@@ -0,0 +1,17 @@
+func cal(_ x: Int) -> Int {
+    if x < 0 {
+        return 0
+    }
+    return x * (x - 1) / 2
+}
+
+class Solution {
+    func distributeCandies(_ n: Int, _ limit: Int) -> Int {
+        return (
+            cal(n + 2)
+            - 3 * cal(n - limit + 1)
+            + 3 * cal(n - (limit + 1) * 2 + 2)
+            - cal(n - 3 * (limit + 1) + 2)
+        )
+    }
+}
