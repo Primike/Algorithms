7e6f266c87d76fa67a471277299f5f23bc19a9a1
Prince Avecillas <avecillasprince@yahoo.com>
2025-07-07 23:22:20 -0400

    Editing

--- swift/Leetcode/Queue/Hard/2014.LongestSubsequence.swift
+++ swift/Leetcode/Queue/Hard/2014.LongestSubsequence.swift
@@ -1,51 +1,48 @@
-class Solution {
-    func longestSubsequenceRepeatedK(_ s: String, _ k: Int) -> String {
-        var ans = ""
-        
-        var freqs: [Character: Int] = [:]
-        for char in s {
-            freqs[char, default: 0] += 1
-        }
-        
-        let candidateChars = freqs.filter { $0.value >= k }.keys.sorted(by: >)
-        
-        var q: [String] = candidateChars.map { String($0) }
-        
-        while !q.isEmpty {
-            let curr = q.removeFirst()
-            
-            if curr.count > ans.count {
-                ans = curr
-            }
-            
-            for char in candidateChars {
-                let nxt = curr + String(char)
-                let target = String(repeating: nxt, count: k)
-                
-                if isSubsequence(target, s) {
-                    q.append(nxt)
-                }
-            }
-        }
-        
-        return ans
+// You are given a string s of length n, and an integer k. 
+// You are tasked to find the longest subsequence repeated k times in string s.
+// A subsequence seq is repeated k times in the string s if seq * k 
+// is a subsequence of s, where seq * k represents a string constructed 
+// by concatenating seq k times.
+// Return the longest subsequence repeated k times in string s. 
+// If multiple such subsequences are found, return the 
+// lexicographically largest one. If there is no such subsequence, 
+// return an empty string.
+
+func longestSubsequenceRepeatedK(_ s: String, _ k: Int) -> String {
+    var dict = s.reduce(into: [:]) { $0[$1, default: 0] += 1 }
+    var validChars = [String]()
+
+    for (key, value) in dict {
+        if value >= k { validChars.append(String(key)) }
     }
 
-    private func isSubsequence(_ sub: String, _ main: String) -> Bool {
-        var subIndex = sub.startIndex
-        if subIndex == sub.endIndex {
-            return true
-        }
+    validChars.sort { $0 > $1 }
+    var queue = validChars
+    var result = ""
+
+    while !queue.isEmpty {
+        let first = queue.removeFirst()
+        if first.count > result.count { result = first }
         
-        for mainChar in main {
-            if mainChar == sub[subIndex] {
-                subIndex = sub.index(after: subIndex)
-                if subIndex == sub.endIndex {
-                    return true
+        for char in validChars {
+            let next = first + char
+            let target = Array(String(repeating: next, count: k))
+            var i = 0
+            
+            for char in s {
+                if char == target[i] { i += 1 }
+    
+                if i == target.count { 
+                    queue.append(next)
+                    break 
                 }
             }
         }
-        
-        return false
     }
+    
+    return result
 }
+
+print(longestSubsequenceRepeatedK("letsleetcode", 2))
+print(longestSubsequenceRepeatedK("bb", 2))
+print(longestSubsequenceRepeatedK("ab", 2))
